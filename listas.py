39e13f54d8d7074bbcdba9e60fc85218ba2a1bf7
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 <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>
2022-10-07 18:27:23 -0300

    hola

---
+++ listas.py
@@ -0,0 +1,12 @@
+import unittest
+listaDeNumeros = [1,2,3,4,3,5,6,7,8,9,4,2,2]
+
+def encontrar_Numeros_Repetidos(listaDeNumeros):
+    diccionarioDeValores={}
+    for element in listaDeNumeros: 
+        if element in diccionarioDeValores:
+            diccionarioDeValores[element] += 1
+        else: 
+            diccionarioDeValores[element] = 1
+
+    return diccionarioDeValores
---
+++ test_listas.py
@@ -0,0 +1,11 @@
+import unittest
+from listas import encontrar_Numeros_Repetidos
+class Test_Palindromo(unittest.TestCase):
+    def test_NumerosRepetidos_1(self):
+        diccionario = encontrar_Numeros_Repetidos([1,2,2,3,4,4,4])
+        self.assertEqual(diccionario,{1:1, 2:2, 3:1, 4:3})
+    def test_NumerosRepetidos_2(self):
+        diccionario = encontrar_Numeros_Repetidos([1,2,2,3,4,4,4])
+        self.assertEqual(diccionario,{1:1, 2:2, 3:1, 4:3})
+if __name__ == '__main__':
+    unittest.main()
