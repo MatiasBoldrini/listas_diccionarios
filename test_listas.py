f5b292f911b03707089b3d8947d532c1e486af37
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 <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>
2022-10-17 11:39:26 -0300

    fixed formatting errors

--- test_listas.py
+++ test_listas.py
@@ -1,11 +1,17 @@
 import unittest
+
 from listas import *
+
+
 class Test_Palindromo(unittest.TestCase):
     def test_NumerosRepetidos_1(self):
-        diccionario = encontrar_Numeros_Repetidos([1,2,2,3,4,4,4])
-        self.assertEqual(diccionario,{1:1, 2:2, 3:1, 4:3})
+        diccionario = encontrar_Numeros_Repetidos([1, 2, 2, 3, 4, 4, 4])
+        self.assertEqual(diccionario, {1: 1, 2: 2, 3: 1, 4: 3})
+
     def test_NumerosRepetidos_2(self):
-        diccionario = encontrar_Numeros_Repetidos([1,2,2,3,4,4,4])
-        self.assertEqual(diccionario,{1:1, 2:2, 3:1, 4:3})
-if __name__ == '__main__':
+        diccionario = encontrar_Numeros_Repetidos([1, 2, 2, 3, 4, 4, 4])
+        self.assertEqual(diccionario, {1: 1, 2: 2, 3: 1, 4: 3})
+
+
+if __name__ == "__main__":
     unittest.main()
